02c06419228a2d9b859383142056b8d934a7ee13
Reena <rinapanchbhai@gmail.com>
2022-07-01 10:14:35 +0530

    added more

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld{
 		System.out.println("Hello World successful");
 		int a = 3+4;
 		int b = 5-3;
+		int c = 2*3;
+		int d = 4/2;
 		System.out.println("Addition="+a);
 		System.out.println("Sub="+b);
+		System.out.println("Multiplication="+b);
+		System.out.println("Division="+b);
 	}
 }
