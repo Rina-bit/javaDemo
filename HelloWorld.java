52dde88b2dd6259eb1fef23b74898232109caf8a
Reena <rinapanchbhai@gmail.com>
2022-07-01 09:47:50 +0530

    calculator added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World successful");
+		int a = 3+4;
+		int b = 5-3;
+		System.out.println("Addition="+a);
+		System.out.println("Sub="+b);
 	}
 }
