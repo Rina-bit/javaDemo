3602e7dd1ce867b6dcb41d4a2854af486ced12f5
Rina-bit <107831294+Rina-bit@users.noreply.github.com>
2022-08-19 16:06:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World successful");
-		int a = 3+4;
+		int a = 3+4
 		int b = 5-3;
 		int c = 2*3;
 		int d = 4/2;
